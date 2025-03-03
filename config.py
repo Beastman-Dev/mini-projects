bde69cc969fbd06e616d023cc6de1e3d792298e6
Beastman-dev <wmeastman@gmail.com>
2025-03-03 11:55:11 -0500

    Completed room dictionaries in config.py

--- config.py
+++ config.py
@@ -37,30 +37,30 @@ room_3 = {
 }
 
 room_4 = {
-    "name": ""
-    "description": "",
-    "search_results": "",
-    "exits": (),
-    "enemies": {},
-    "treasure": {}
+    "name": "Common Room",
+    "description": "This large room is littered with primitive furniture, decorations, and a number of goblins. Several goblins are eating some foul-smelling meal at a table on one side of the room while another handful sit on makeshift stools in a corner, a pile of bones and coins on the floor between them.",
+    "search_results": "The furnishing and food in this room are worthless and unappetizing, but you find a dozen copper on the floor next to the bones and on the various goblins.",
+    "exits": ("North-East", "North-West", "South"),
+    "enemies": {"goblins": 7, "goblin warriors": 2},
+    "treasure": {"copper": 12}
 }
 
 room_5 = {
-    "name": ""
-    "description": "",
-    "search_results": "",
-    "exits": (),
-    "enemies": {},
-    "treasure": {}
+    "name": "Throne Room Foyer"
+    "description": "Hall with double doors at both ends and two tough-looking goblins standing guard in front of the doors to the South.",
+    "search_results": "You hear faint sounds of voices coming from the other side of the Southern double-doors.",
+    "exits": ("North", "South"),
+    "enemies": {"goblin warriors": 2},
+    "treasure": {"silver": 4}
 }
 
 room_6 = {
-    "name": ""
-    "description": "",
-    "search_results": "",
-    "exits": (),
-    "enemies": {},
-    "treasure": {}
+    "name": "Throne Room"
+    "description": "Large room with four pillars in the middle and a raised dais on the South side. The only entrance is the double doors you entered through. On the dais sits a rickety throne decorated with bones, an ugly statue of a goblin, and a large wodden chest. A fat goblin sits on the throne, a garishly decorated crown on his head. His clothing is of finer material than the others but tattered, poorly sewn rips in several places. Three other goblins stand near the throne. Standing to the king's right is a large, muscular goblin wearing leather armor and carrying a spiked club. To his left stands a slim, older-looking goblin with an ornate staff and wearing robes. Kneeling at the base of the steps to the dias is a female goblin wearing a dress.",
+    "search_results": "The chest contains 11 gold, 27 silver, and 59 copper.",
+    "exits": ("North"),
+    "enemies": {"goblin king": 1, "goblin champion": 1, "goblin shaman": 1, "goblin female": 1},
+    "treasure": {"gold": 11, "silver": 27, "copper": 59}
 }
 
 r1_description = ""
