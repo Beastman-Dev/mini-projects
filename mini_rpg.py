8687752bc5e9037cda41a74ab7e080a7fe18a12a
Beastman-dev <wmeastman@gmail.com>
2025-03-04 16:31:14 -0500

    More unit test cleanup

--- mini_rpg.py
+++ mini_rpg.py
@@ -48,9 +48,6 @@ class Sorcerer(Creature):
         self.current_mana -= spell_power
         self.damage_target(target, spell_power)
         return spell_power
-        # target.health -= self.attack
-        # self.mana -= 10
-        # return target.health
 
 class Treasure:
     def __init__(self, name, description, value):
@@ -99,31 +96,6 @@ class Potion(Treasure):
 
 
 
-
-# Creature unit tests
-# enemy: Monster = Monster("Goblin 1", 50, 5)
-# print(f"{enemy.name} - Health: {enemy.health} - Attack: {enemy.attack}")
-# character: Character = Character("Beastman", 100, 10)
-# print(f"{character.name} - Health: {character.health} - Attack: {character.attack}")
-# sorcerer: Sorcerer = Sorcerer("Gandalf", 100, 10, 100)
-# print(f"{sorcerer.name} - Health: {sorcerer.health} - Attack: {sorcerer.attack} - Mana: {sorcerer.mana}")
-
-# Attack unit tests
-# character: Character = Character("Beastman", 100, 10)
-# enemy: Monster = Monster("Goblin 1", 50, 5)
-# character.damage_target(enemy, character.attack)
-# print(f"{character.name} attacks {enemy.name} for {character.attack}.")
-# print(f"{enemy.name}'s health is now {enemy.health}.")
-
-# Cast spell unit tests
-# enemy: Monster = Monster("Goblin 1", 50, 5)
-# sorcerer: Sorcerer = Sorcerer("Gandalf", 100, 10, 100)
-# spell_power = sorcerer.cast_spell(enemy)
-# print(f"{sorcerer.name} casts a spell on {enemy.name} for {spell_power}.")
-# print(f"{enemy.name}'s health is now {enemy.health}.")
-# print(f"{sorcerer.name}'s mana is now {sorcerer.mana}.")
-
-
 ## Unit Tests
 
 def create_dungeon_unit_tests():
@@ -168,11 +140,55 @@ def use_potion_unit_tests():
         health_potion: Potion = Potion("health", 10, 10)
         mana_potion: Potion = Potion("mana", 10, 10)
         health_potion.use_potion(fighter)
+        if fighter.current_health == 100:
+            test_1 = True
+        else:
+            test_1 = False
         mana_potion.use_potion(sorcerer)
-        if fighter.current_health == 100 and sorcerer.current_mana == 100:
+        if sorcerer.current_mana == 100:
+            test_2 = True
+        else:
+            test_2 = False
+        fighter.current_health = 80
+        sorcerer.current_mana = 80
+        health_potion.use_potion(fighter)
+        if fighter.current_health == 90:
+            test_3 = True
+        else:
+            test_3 = False
+        mana_potion.use_potion(sorcerer)
+        if sorcerer.current_mana == 90:
+            test_4 = True
+        else:
+            test_4 = False
+        if test_1 == True and test_2 == True and test_3 == True and test_4 == True:
             return True
     except Exception as e:
         return e
+
+def attack_unit_tests():
+    try:
+        character: Character = Character("Beastman", 100, 10)
+        enemy: Monster = Monster("Goblin 1", 50, 5)
+        character.damage_target(enemy, character.attack)
+        if enemy.current_health == 40:
+            return True
+    except Exception as e:
+        return e
+
+def cast_spell_unit_tests():
+    try:
+        enemy: Monster = Monster("Goblin 1", 50, 5)
+        sorcerer: Sorcerer = Sorcerer("Gandalf", 100, 10, 100)
+        spell_power = sorcerer.cast_spell(enemy)
+        if enemy.current_health == 45 and sorcerer.current_mana == 90:
+            return True
+    except Exception as e:
+        return e
+
+
+
+
 
 ## Dungeon creation tests
 # passed = create_dungeon_unit_tests()
@@ -214,19 +230,23 @@ def use_potion_unit_tests():
 # if passed != True:
 #     print(f"Error: {passed}")
 
+## Potion use tests
+# passed = use_potion_unit_tests()
+# if passed == True:
+#     result = "PASSED"
+# else:
+#     result = "FAILED"
+# print(f"Potion use unit test: {result}")
+# if passed != True:
+#     print(f"Error: {passed}")
 
-
-# fighter: Character = Character("Beastman", 100, 10)
-# sorcerer: Sorcerer = Sorcerer("Gandalf", 100, 10, 100)
-
-##   Use potions
-# print(f"{fighter.name} has {fighter.health} HP.")
-# health_potion.use_potion(fighter)
-# print(f"{fighter.name}'s health is now {fighter.health}.")
-
-# print(f"{sorcerer.name} has {sorcerer.mana} Mana.")
-# mana_potion.use_potion(sorcerer)
-# print(f"{sorcerer.name}'s mana is now {sorcerer.mana}.")
-
-# mana_potion.use_potion(fighter)
+## Attack tests
+# passed = attack_unit_tests()
+# if passed == True:
+#     result = "PASSED"
+# else:
+#     result = "FAILED"
+# print(f"Attack unit test: {result}")
+# if passed != True:
+#     print(f"Error: {passed}")
 
