a6598e84d0ef8460abae5427c89e186ecc8cb46e
Beastman-dev <wmeastman@gmail.com>
2025-03-04 09:28:25 -0500

    Created unit tests for Dungeon and Creature classes and attack method.

--- mini_rpg.py
+++ mini_rpg.py
@@ -16,14 +16,23 @@ class Room:
         self.treasure = treasure
 
 class Creature:
-    def __init__(self, name, health):
+    def __init__(self, name, health, attack):
         self.name = name
         self.health = health
+        self.attack = attack
+
+    def damage_target(self, target):
+        self.target = target
+        target.health -= self.attack
+        return target.health
 
 class Monster(Creature):
     pass
 
 class Character(Creature):
+    pass
+
+class Sorcerer(Creature):
     pass
 
 class Treasure:
@@ -38,7 +47,19 @@ class Weapon(Treasure):
 class Armor(Treasure):
     pass
 
-dungeon: Dungeon = Dungeon("Tomb of Minor Inconvenience", config.dungeon_rooms)
-print(f"WELCOME to the {dungeon.name}!")
-print(f"You enter the {dungeon.rooms["room_6"]["name"]}.")
-print(dungeon.rooms["room_6"]["description"])
+# Dungeon unit tests
+# dungeon: Dungeon = Dungeon("Tomb of Minor Inconvenience", config.dungeon_rooms)
+# print(f"WELCOME to the {dungeon.name}!")
+# print(f"You enter the {dungeon.rooms["room_6"]["name"]}.")
+# print(dungeon.rooms["room_6"]["description"])
+
+# Creature unit tests
+enemy: Monster = Monster("Goblin 1", 50, 5)
+print(f"{enemy.name} - Health: {enemy.health} - Attack: {enemy.attack}")
+character: Character = Character("Beastman", 100, 10)
+print(f"{character.name} - Health: {character.health} - Attack: {character.attack}")
+
+# Attack unit tests
+character.damage_target(enemy)
+print(f"{character.name} attacks {enemy.name} for {character.attack}.")
+print(f"{enemy.name}'s health is now {enemy.health}.")
