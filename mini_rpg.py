b8045566bc3cc617a5e44926b0e26a5a10ab684c
Beastman-dev <wmeastman@gmail.com>
2025-03-04 09:58:54 -0500

    Working on sorcerer class attack function

--- mini_rpg.py
+++ mini_rpg.py
@@ -21,9 +21,9 @@ class Creature:
         self.health = health
         self.attack = attack
 
-    def damage_target(self, target):
+    def damage_target(self, target, attack_power):
         self.target = target
-        target.health -= self.attack
+        target.health -= attack_power
         return target.health
 
 class Monster(Creature):
@@ -33,7 +33,22 @@ class Character(Creature):
     pass
 
 class Sorcerer(Creature):
-    pass
+    def __init__(self, name, health, attack, mana):
+        super().__init__(name, health, attack)
+        self.mana = mana
+
+    def cast_spell(self, target):
+        self.target = target
+        spell_power = int(input("How much mana would you like to spend? "))
+        if spell_power > self.mana:
+            print("You don't have enough mana!")
+            return
+        self.mana -= spell_power
+        self.damage_target(target, spell_power)
+        return spell_power
+        # target.health -= self.attack
+        # self.mana -= 10
+        # return target.health
 
 class Treasure:
     def __init__(self, name, description, value):
@@ -58,8 +73,16 @@ enemy: Monster = Monster("Goblin 1", 50, 5)
 print(f"{enemy.name} - Health: {enemy.health} - Attack: {enemy.attack}")
 character: Character = Character("Beastman", 100, 10)
 print(f"{character.name} - Health: {character.health} - Attack: {character.attack}")
+sorcerer: Sorcerer = Sorcerer("Gandalf", 100, 10, 100)
+print(f"{sorcerer.name} - Health: {sorcerer.health} - Attack: {sorcerer.attack} - Mana: {sorcerer.mana}")
 
 # Attack unit tests
-character.damage_target(enemy)
-print(f"{character.name} attacks {enemy.name} for {character.attack}.")
+# character.damage_target(enemy, character.attack)
+# print(f"{character.name} attacks {enemy.name} for {character.attack}.")
+# print(f"{enemy.name}'s health is now {enemy.health}.")
+
+# Cast spell unit tests
+spell_power = sorcerer.cast_spell(enemy)
+print(f"{sorcerer.name} casts a spell on {enemy.name} for {spell_power}.")
 print(f"{enemy.name}'s health is now {enemy.health}.")
+print(f"{sorcerer.name}'s mana is now {sorcerer.mana}.")
